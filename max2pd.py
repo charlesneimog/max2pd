fe3002b98f2ef1e86503e8d2b57090b5c75005af
Charles K. Neimog <charlesneimog@outlook.com>
2022-05-08 22:06:12 -0300

    learning yet how puredata works

--- max2pd.py
+++ max2pd.py
@@ -5,6 +5,18 @@ import json
 with open("max2pd.json") as json_file:
     max2pd_objects = json.load(json_file)
 
+
+class pcolors:
+    HEADER = '\033[95m'
+    BLUE = '\033[94m'
+    CYAN = '\033[96m'
+    GREEN = '\033[92m'
+    ORANGE = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
 # Opening JSON file
 PATCH = []
 INDEX = 0
@@ -14,59 +26,69 @@ with open("max2pd.maxpat") as json_file:
     
 
 def max2pd_objects(json_data):
+    subpatcher = len(json_data.keys())
     key = 'patcher'
     patcher = json_data[key]
     canvas = json_data[key]['rect']
     canvas_string = ''
     for i in canvas: 
         canvas_string += str(i) + ' '
-    canvas_string = '#N canvas ' + canvas_string + ' ' + '10' + ';'
+    if subpatcher == 1:
+        canvas_string = '#N canvas ' + canvas_string + ' ' + '10' + ';'
+    else:
+        subpatcher_name = json_data['text']
+        canvas_string = '#N canvas ' + canvas_string + '0 ' + subpatcher_name + ';'
+    
+    
     PATCH.append(canvas_string)
     boxes = patcher['boxes']
     lines = patcher['lines']
     for box in boxes:
+        print('-----------------------------------------------------')
         get_box_id = str(box['box']['id'])
         get_box_id = get_box_id.split('obj-')[1]
-        
         if box['box']['maxclass'] == 'newobj': # Objetos
             x_y_position = box['box']['patching_rect'][:2]
             x_y_position = str(x_y_position[0]) + " " + str(x_y_position[1])
             OBJECT_NAME = box['box']['text']
             OBJECT_NAME = OBJECT_NAME.split(' ')[0]
             try:
-                PD_OBJECT_NAME = max2pd_objects['max2pd'][OBJECT_NAME]
+                PD_OBJECT_NAME = max2pd_objects['max2pd'][OBJECT_NAME] ## Check if there is a match object in the community 
             except:
                 try:
                     PD_OBJECT_NAME = max2pd_objects['pdobjects'][OBJECT_NAME]
+                    
                 except:
                     if OBJECT_NAME == 'p':
-                        print('Subpatch')
                         max2pd_objects(box['box']) # Aqui vai a recursão
                             
                     else:
-                        print('Object not found: ' + OBJECT_NAME)
-                        print(box['box']['maxclass'])
-
-
+                        print(f'{pcolors.FAIL}Object not found: ' + f'{pcolors.BLUE}' + OBJECT_NAME + f'{pcolors.ENDC}')
+                        continue
                     PD_OBJECT_NAME = OBJECT_NAME
-                
             OBJECT_ARGS = box['box']['text'].split(' ')[1:]
             OBJECT_ARGS = ' '.join(OBJECT_ARGS)
-
-            
             VALUE = '#X obj ' + x_y_position + ' ' + PD_OBJECT_NAME + ' ' + OBJECT_ARGS + ' ;'
             PATCH.append(VALUE)
-
 
         elif box['box']['maxclass'] == 'slider': # Sliders
             x_y_position = box['box']['patching_rect'][:2]
             x_y_position = str(x_y_position[0]) + str(x_y_position[1])
         
-
         elif box['box']['maxclass'] == 'button': # Bang e Toggles
             x_y_position = box['box']['patching_rect'][:2]
             x_y_position = str(x_y_position[0]) + str(x_y_position[1])
 
+        elif box['box']['maxclass'] == 'inlet':
+            x_y_position = box['box']['patching_rect'][:2]
+            x_y_position = str(x_y_position[0]) + " " + str(x_y_position[1])
+            VALUE = "#X " + "obj" + " " + x_y_position + " " + "inlet" + " " + ";"
+            PATCH.append(VALUE)
+        elif box['box']['maxclass'] == 'outlet':
+            x_y_position = box['box']['patching_rect'][:2]
+            x_y_position = str(x_y_position[0]) + " " + str(x_y_position[1])
+            VALUE = "#X " + "obj" + " " + x_y_position + " " + "outlet" + " " + ";"
+            PATCH.append(VALUE)
 
         elif box['box']['maxclass'] == 'message': # Mensagens
             x_y_position = box['box']['patching_rect'][:2]
@@ -82,28 +104,27 @@ def max2pd_objects(json_data):
         else: 
             x_y_position = box['box']['patching_rect'][:2]
             x_y_position = str(x_y_position[0]) + str(x_y_position[1])
-            print('NEED TO BE IMPLEMENTEND: ', box['box']['maxclass'])
+            print(f'{pcolors.FAIL}NEED TO BE IMPLEMENTEND: ', box['box']['maxclass'] , pcolors.ENDC)
             x_y_position = box['box']['patching_rect'][:2]
-            print('-----------------------------------------------------')
+        
+        
 
 
     for line in lines:
         destination = line['patchline']['destination']
         source = line['patchline']['source']
     
-
+    ## HOW TO KNOW THAT I AM INSIDE A SUBPATCHER??
     return PATCH
 
 
 PATCH = max2pd_objects(data)
 
 
-for item in PATCH:
-    print(item)
+# for item in PATCH:
+#     print(item)
 
-
-# Writing to file
-# with open("Max2PD.pd", "w") as f:
-#     for item in PATCH:
-#         f.write(item + "\n")
-# f.close()
+with open("Max2PD.pd", "w") as f:
+    for item in PATCH:
+        f.write(item + "\n")
+f.close()
