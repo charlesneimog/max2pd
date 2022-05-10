247a6f73b8fc671d4a74ed46bd3d7f66179ef902
Charles K. Neimog <charlesneimog@outlook.com>
2022-05-10 10:56:44 -0300

    remove garbage

--- Test.py
+++
@@ -1,9 +0,0 @@
-with open('objects_not_found.txt') as f:
-    # if file not exists, create it
-    lines = f.readlines()
-
-
-# remove \n from the end of each line
-lines = [line.rstrip('\n') for line in lines]
-
-print(lines)
