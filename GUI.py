089bc4525f495b641fad25177e03414a8bcaaffa
Charles K. Neimog <charlesneimog@outlook.com>
2022-05-10 15:32:35 -0300

    add a Simple_GUI

---
+++ GUI.py
@@ -0,0 +1,96 @@
+from tkinter import *
+from tkinter import filedialog
+from tkinter import messagebox
+import os
+import sys
+from max2pd import * 
+
+script_path = os.path.dirname(sys.argv[0])
+
+
+# ================================================== Acabou de criar a interface ==================================================
+def exit_call():
+    res = messagebox.askquestion('Convert new Patch', 'You want to convert a new Patch?')
+    if res == 'no' :
+        root.destroy()
+    else :
+        patch_chooser_dialog()
+
+
+def patch_chooser_dialog():
+    global FILE
+    patch_chooser = Tk()
+    patch_chooser.iconbitmap(f'{script_path}\\opensource.ico')
+    patch_chooser.title('Max2PD')
+    patch_chooser.geometry("480x400+715+200")
+    patch_chooser.withdraw()
+    FILE = filedialog.askopenfilename(filetypes=[("Max patch files", "*.maxpat")])
+    if FILE == '':
+        print_in_thinker(f'No file selected.', 'red', root)
+        print_in_thinker(f'Exiting...', 'red', root)
+        time.sleep(0.2)
+        root.destroy()
+        sys.exit()
+    else:
+        print(f'{pcolors.GREEN} File selected: {FILE} {pcolors.ENDC}')
+
+    patch_chooser.destroy()
+    convert2pd(FILE, root)
+    if THERE_IS_ERRORS:
+        with open('objects_not_found.txt') as f:
+            lines = f.readlines()
+        lines = [line.rstrip('\n') for line in lines]
+        seen = set()
+        result = []
+        for item in lines:
+            if item not in seen:
+                seen.add(item)
+                result.append(item)
+
+        try:
+            os.remove('objects_not_found.txt')
+        except:
+            pass
+
+        if result != []:
+            with open('PLEASE_REPORT_missing_objects.txt', 'w') as f:
+                for line in result:
+                    f.write(line + '\n')
+        
+    exit_call()
+
+def on_closing():
+    root.destroy()
+    sys.exit()
+
+def close_window(): 
+    destroy()
+
+
+## Create Main Window ==================================================
+global root
+root = Tk()
+root.iconbitmap(f'{script_path}\\opensource.ico')
+root.title('Max2PD')
+screen_width = root.winfo_screenwidth()
+screen_height = root.winfo_screenheight()
+window_height = 150
+window_width = 430
+x_cordinate = int((screen_width/2) - (window_width/2))
+y_cordinate = int((screen_height/2) - (window_height/2))
+
+root.geometry("{}x{}+{}+{}".format(window_width, window_height, x_cordinate, y_cordinate))
+
+root.bind('<Return>', lambda event=None: answer_button.invoke())
+root.bind('<Escape>', lambda event=None: finish_button.invoke())
+
+# Create Buttons
+button_frame = Frame(root)
+button_frame.pack(pady=20)
+answer_button = Button(button_frame, text="Choose Max-Patch", command=patch_chooser_dialog)
+answer_button.grid(row=0, column=0, padx=20)
+
+
+root.protocol("WM_DELETE_WINDOW", on_closing)
+
+root.mainloop()
--- max2pd.py
+++ max2pd.py
@@ -3,6 +3,7 @@ import tkinter as tk
 from tkinter import filedialog
 import os
 import sys
+import time
 
 THERE_IS_ERRORS = False
 
@@ -17,53 +18,31 @@ class pcolors:
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
 
-welcome_string = '''
 
-                                         
-     ____    ____      _      ____  ____    _____    _______ ______    
-    |_   \  /   _|    / \    |_  _||_  _|  / ___ `. |_   __ |_   _ `.  
-      |   \/   |     / _ \     \ \  / /   |_/___) |   | |__) || | `. \ 
-      | |\  /| |    / ___ \     > `' <     .'____.'   |  ___/ | |  | | 
-     _| |_\/_| |_ _/ /   \ \_ _/ /'`\ \_  / /_____   _| |_   _| |_.' / 
-    |_____||_____|____| |____|____||____| |_______| |_____| |______.'  
-                                                                   
+def print_in_thinker(string, color, root):
+    # Draw the string always on the same line
+    root.title(string)
+    msg = tk.Message(root, text = string)
+    msg.config(fg=color, font=('times', 12), width=500, justify='center')
+    # msg pack in the same line
+    msg.pack()
+    root.update()
+    msg.destroy()
+    time.sleep(1)
 
-                                      version 0.0.1
-==========================================================================================
-                 A tool will convert all the Max patches to PD patches.
-==========================================================================================
-                This tool is a free software distributed under the GPLv3 license. 
-------------------------------------------------------------------------------------------
-
-'''
-
-print(welcome_string)
-
-print(f'{pcolors.BLUE} Please, select the Max patch file: {pcolors.ENDC}')
-
-import time
-time.sleep(1.5)
-
-root = tk.Tk()
-root.withdraw()
-file_path = filedialog.askopenfilename(filetypes=[("Max patch files", "*.maxpat")])
-FILE = file_path
-
-if FILE == '':
-    print(f'{pcolors.FAIL} No file selected. {pcolors.ENDC}')
-    print(f'{pcolors.FAIL} Exiting... {pcolors.ENDC}')
-    time.sleep(1.4)
-    sys.exit()
-    
 # ====================================
 
-def convert2pd(FILE):
+def convert2pd(FILE, TK_GUI):
     with open(FILE) as json_file:
         data = json.load(json_file)
-
-    PATCH = max2pd(data)
+    # took the folder of FILE
+    MAIN_FOLDER = os.path.dirname(FILE)
+    PATCH = max2pd(data, MAIN_FOLDER, TK_GUI)
     PD_FILE = os.path.splitext(FILE)[0] + '.pd'
-    print(f'{pcolors.BLUE} PD patch will be saved in: {pcolors.ENDC}', PD_FILE)
+    # GET NAME OF PATCH
+    PATCH_MSG = os.path.basename(FILE)
+    PATCH_MSG = PATCH_MSG.replace('.maxpat', '')
+    print_in_thinker(f'PD patch saved in:  {PATCH_MSG}', 'green', TK_GUI)
     with open(PD_FILE, "w") as f:
         for item in PATCH:
             f.write(item + "\n")
@@ -72,10 +51,11 @@ def convert2pd(FILE):
 
 # ====================================
 
-def max2pd(json_data):
+def max2pd(json_data, MAIN_FOLDER, TK_GUI):
     with open("max2pd.json") as json_file:
         max2pd_objects = json.load(json_file)
     INDEX = 0
+    MULTISLIDER_INDEX = 0
     PATCH = []
     OBJECTS_ID = {}
     subpatcher = len(json_data.keys())
@@ -95,7 +75,7 @@ def max2pd(json_data):
     boxes = patcher['boxes']
     lines = patcher['lines']
     for box in boxes:
-        sub_patch = False
+        special_case = False
         get_box_id = str(box['box']['id'])
         OBJECTS_ID[get_box_id] = INDEX
         INDEX = INDEX + 1
@@ -112,45 +92,54 @@ def max2pd(json_data):
                     PD_OBJECT_NAME = max2pd_objects['pdobjects'][OBJECT_NAME] ## Check if it is one PD object
             
                 except: # if it is not a PD object, it is a subpatcher (p) or a poly~
+                    ## SOME SPECIAL CASES
+                                        
                     if OBJECT_NAME == 'p':
                         subpatcher_name = box['box']['text']
                         subpatcher_name = subpatcher_name.split('p')[1]
-                        print(f'{pcolors.BLUE}Converting {subpatcher_name} subpatch...{pcolors.ENDC}')
-                        sub_patch = max2pd(box['box']) # Aqui vai a recursão
+                        print_in_thinker(f' Converting {subpatcher_name} subpatch... ', 'green', TK_GUI)
+                        sub_patch = max2pd(box['box'], MAIN_FOLDER, TK_GUI) # Aqui vai a recursão
                         restore_window = '#X restore ' + x_y_position + ' pd ' + subpatcher_name + ';'
                         sub_patch.append(restore_window)
                         for line in sub_patch:
                             PATCH.append(line)
-                        sub_patch = True
-                        print(f'{pcolors.BLUE}Subpatch {subpatcher_name} done!{pcolors.ENDC}')
+                        special_case = True
+                        print_in_thinker(f'Subpatch {subpatcher_name} done! ', 'green', TK_GUI)
                     
+                    elif OBJECT_NAME == 'in':
+                        special_case = True
+                        VALUE = '#X obj ' + x_y_position + ' ' + "inlet" + ' ' ' ;'
+                        PATCH.append(VALUE)
+                    
+                    elif OBJECT_NAME == 'out~':
+                        special_case = True
+                        VALUE = '#X obj ' + x_y_position + ' ' + "outlet~" + ' ' ' ;'
+                        PATCH.append(VALUE)
+                        
                     elif OBJECT_NAME == 'poly~':
                         subpatcher_name = box['box']['text']
                         subpatcher_name = subpatcher_name.split('poly~ ')[1]
                         subpatcher_name = subpatcher_name.split(' ')[0]
-                        print(f'{pcolors.BLUE}Converting {subpatcher_name} subpatch...{pcolors.ENDC}')
-                        folder = os.path.dirname(FILE)
+                        print_in_thinker(f' Converting {subpatcher_name} subpatch... ', 'green', TK_GUI)
+
                         # Folder + OBJECT_NAME
-                        subpatch_filename = folder + '/' + subpatcher_name + '.maxpat'
-                        print(subpatch_filename)
+                        subpatch_filename = MAIN_FOLDER + '/' + subpatcher_name + '.maxpat'
                         if os.path.isfile(subpatch_filename):
-                            convert2pd(subpatch_filename)
+                            convert2pd(subpatch_filename, TK_GUI)
                         else:
-                            print(f'{pcolors.FAIL}Sorry, the poly~ subpath not found: ' + f'{pcolors.BLUE}' + OBJECT_NAME + f'{pcolors.ENDC}')
-                        # Aqui vai a recursão
+                            print_in_thinker(f'Sorry, the poly~ subpath not found: {OBJECT_NAME}', 'red', TK_GUI)
                         clone_obj = '#X obj ' + x_y_position + " clone " + subpatcher_name + " 32" + ';'
                         PATCH.append(clone_obj)
-                        sub_patch = True                   
+                        special_case = True                   
                     
                     else:    
-                        # Get folder of FILE
-                        folder = os.path.dirname(FILE)
-                        # Folder + OBJECT_NAME
-                        subpatch_filename = folder + '/' + OBJECT_NAME + '.maxpat'
+                       # Folder + OBJECT_NAME
+                        subpatch_filename = MAIN_FOLDER + '/' + OBJECT_NAME + '.maxpat'
                         if os.path.isfile(subpatch_filename):
-                            convert2pd(subpatch_filename)
+                            convert2pd(subpatch_filename, TK_GUI)
                         else:                      
-                            print(f'{pcolors.FAIL}Object not found: ' + f'{pcolors.BLUE}' + OBJECT_NAME + f'{pcolors.ENDC}')
+
+                            print_in_thinker(f'Object not found: {OBJECT_NAME}', 'red', TK_GUI)
                             with open('objects_not_found.txt', 'a') as f:
                                 f.write(OBJECT_NAME + '\n')
                             global THERE_IS_ERRORS
@@ -158,16 +147,74 @@ def max2pd(json_data):
                           
                     PD_OBJECT_NAME = OBJECT_NAME
             
-            if sub_patch == False:
+            if special_case == False:
                 OBJECT_ARGS = box['box']['text'].split(' ')[1:]
                 OBJECT_ARGS = ' '.join(OBJECT_ARGS)
                 VALUE = '#X obj ' + x_y_position + ' ' + PD_OBJECT_NAME + ' ' + OBJECT_ARGS + ' ;'
                 PATCH.append(VALUE)
 
-        # elif box['box']['maxclass'] == 'slider': # Sliders
-        #     x_y_position = box['box']['patching_rect'][:2]
-        #     x_y_position = str(x_y_position[0]) + str(x_y_position[1])
+        elif box['box']['maxclass'] == 'slider': # Sliders
+            x_y_position = box['box']['patching_rect'][:2]
+            x_y_position = str(x_y_position[0]) + " " + str(x_y_position[1])
+            tamanho = 15
+            VALUE = '#X obj ' + x_y_position + ' ' + 'hsl 128 ' + str(tamanho) + ' 0 127 0 0 empty empty empty -2 -8 0 10 -262144 -1 -1 0 1;'
+            PATCH.append(VALUE)
         
+        elif box['box']['maxclass'] == 'multislider': # Multislider
+            MINMAX = box['box']['setminmax']
+            
+            ## SUBPATCH
+            x_y_position = box['box']['patching_rect'][:2]
+            x_y_position = str(x_y_position[0]) + " " + str(x_y_position[1])
+            multislider_patch = []
+            PD2MAX_multislider_NAME = 'PD2MAX_multislider_' +  str(MULTISLIDER_INDEX)
+            ## START SUBPATCH
+            multislider_patch.append('#N canvas 606 43 1226 621 10;')
+            multislider_patch.append('#X obj 369 76 f \$1;')
+            multislider_patch.append('#X obj 369 31 loadbang;')
+            multislider_patch.append('#X obj 414 76 f \$2;')
+            multislider_patch.append('#X obj 369 52 t b b, f 8;')
+            multislider_patch.append('#N canvas 0 50 450 250 (subpatch) 0;')
+            multislider_patch.append(f'#X array {PD2MAX_multislider_NAME} 100 float 3;')
+            multislider_patch.append('#A 0 0 1 2 3 4 5 6 7 8 9 10 9 8 7 5 4 2 0 -1 -3 -4 -6 -8 -9 -11 -12 -13 -15 -16 -17 -18 -19 -20 -102 -103 -104 -105 -106 -105 -104 -101 -95 -92 -88 -86 -82 -79 -76 -73 -70 -67 -65 -62 -59 -58 -55 -53 -52 -50 -48 -46 -44 -43 -41 -40 -38 -36 -35 -34 -33 -32 -31 -29 -28 -27 -26 -25 -24 -23 -22 -21 -20 -19 -18 -17 -16 -15 -14 -13 -12 -11 -10 -9 -8 -6 -5 -4 -3 -2 0;')
+            multislider_patch.append('#X coords 0 1 100 0 200 140 1 100 100;')
+            multislider_patch.append('#X restore 2 1 graph;')
+            multislider_patch.append('#X obj 340 104 else/rescale 0 1 0 1;')
+            multislider_patch.append('#X obj 340 132 t f b;')
+            multislider_patch.append('#X obj 367 154 int 1;')
+            multislider_patch.append('#X obj 367 183 + 1, f 6;')
+            multislider_patch.append('#X obj 367 209 mod 100;')
+            multislider_patch.append('#X obj 367 230 + 1;')
+            multislider_patch.append(f'#X obj 280 259 tabwrite {PD2MAX_multislider_NAME};')
+            multislider_patch.append('#X obj 333 76 inlet;')
+            multislider_patch.append('#X connect 0 0 5 1;')
+            multislider_patch.append('#X connect 1 0 3 0;')
+            multislider_patch.append('#X connect 2 0 5 2;')
+            multislider_patch.append('#X connect 3 0 0 0;')
+            multislider_patch.append('#X connect 3 1 2 0;')
+            multislider_patch.append('#X connect 5 0 6 0;')
+            multislider_patch.append('#X connect 6 0 11 0;')
+            multislider_patch.append('#X connect 6 1 7 0;')
+            multislider_patch.append('#X connect 7 0 8 0;')
+            multislider_patch.append('#X connect 8 0 9 0;')
+            multislider_patch.append('#X connect 8 0 7 1;')
+            multislider_patch.append('#X connect 9 0 10 0;')
+            multislider_patch.append('#X connect 10 0 11 1;')
+            multislider_patch.append('#X connect 12 0 5 0;')
+            multislider_patch.append('#X coords 0 0 1 1 200 140 1 2 1;')
+            ## END SUBPATCH
+
+            multislider_patch_NAME =  f'multislider_{MULTISLIDER_INDEX}.pd'
+            multislider_patch_FILE = MAIN_FOLDER + '/' + multislider_patch_NAME 
+            MULTISLIDER_INDEX = MULTISLIDER_INDEX + 1
+            print(f'  Multislider abstraction saved in:  ', multislider_patch_NAME)
+            with open(multislider_patch_FILE, "w") as f:
+                for item in multislider_patch:
+                    f.write(item + "\n")
+                f.close()
+            multislider_patch_NAME = multislider_patch_NAME.replace('.pd', '')
+            VALUE = '#X obj ' + x_y_position + ' ' + multislider_patch_NAME + ' ' + str(MINMAX[0]) + ' ' + str(MINMAX[1]) + ' ;', 
+            PATCH.append(VALUE[0])
 
         # numbers
         elif box['box']['maxclass'] == 'number' or box['box']['maxclass'] == 'flonum':
@@ -242,14 +289,17 @@ def max2pd(json_data):
         else: 
             x_y_position = box['box']['patching_rect'][:2]
             x_y_position = str(x_y_position[0]) + str(x_y_position[1])
-            print(f'{pcolors.FAIL}NEED TO BE IMPLEMENTEND: ', box['box']['maxclass'] , pcolors.ENDC)
+            NEED_IMPLEMENTED = box["box"]["maxclass"]
+            print_in_thinker(f'NEED TO BE IMPLEMENTEND: {NEED_IMPLEMENTED}', 'red', TK_GUI)
             x_y_position = box['box']['patching_rect'][:2]
             x_y_position = str(x_y_position[0]) + " " + str(x_y_position[1])
-            # pd += "\n#X " + "obj "+ args[0] + " bogus" + ";";
             VALUE = "#X " + "obj" + " " + x_y_position + " " + "NEED_TO_BE_IMPLEMENTED!" + " ;"
             PATCH.append(VALUE)
-
-                
+        
+        
+        # UPDATE THE GUI
+        
+        
     ## This will change all the 
     for line in lines:
         connections = []
@@ -261,40 +311,7 @@ def max2pd(json_data):
         connections = '#X connect ' + connections + ' ;'
         PATCH.append(connections)
     
-    time.sleep(1.3)
-    print(f'{pcolors.GREEN}Patch created!{pcolors.ENDC}')
+    print_in_thinker('Patch created!', 'green', TK_GUI)
     return PATCH
 
-convert2pd(FILE)
-
-
 ## read 
-
-if THERE_IS_ERRORS:
-    with open('objects_not_found.txt') as f:
-        # if file not exists, create it
-        lines = f.readlines()
-    # remove \n from the end of each line
-    lines = [line.rstrip('\n') for line in lines]
-    # remove duplicates
-
-    seen = set()
-    result = []
-    for item in lines:
-        if item not in seen:
-            seen.add(item)
-            result.append(item)
-    print(result)
-
-    ## delte objects_not_found.txt
-    try:
-        os.remove('objects_not_found.txt')
-    except:
-        pass
-
-    # write lines to file
-    
-    if result != []:
-        with open('PLEASE_REPORT_missing_objects.txt', 'w') as f:
-            for line in result:
-                f.write(line + '\n')
